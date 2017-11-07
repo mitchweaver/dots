d31453c1f84311f4a71f7db2448791045e1ed918
Mitch Weaver <20451170+MitchWeaver@users.noreply.github.com>
2017-11-07 01:09:03 +0100

    a

---
+++ suckless-tools/st/st/config.mk
@@ -0,0 +1,33 @@
+# st version
+VERSION = 0.7
+
+# Customize below to fit your system
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = $(PREFIX)/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+# includes and libs
+INCS = -I$(X11INC) \
+       `pkg-config --cflags fontconfig` \
+       `pkg-config --cflags freetype2`
+
+
+# DEFAULT
+#LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
+ 
+# WITH TRANSPARENCY
+LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft -lXrender\
+	   `pkg-config --libs fontconfig` \
+       `pkg-config --libs freetype2`
+
+# flags
+CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
+STCFLAGS = $(INCS) $(CPPFLAGS) $(CFLAGS)
+STLDFLAGS = $(LIBS) $(LDFLAGS)
+
+# compiler and linker
+# CC = c99
---
+++ suckless-tools/st/st/win.h
@@ -0,0 +1,34 @@
+/* See LICENSE for license details. */
+
+/* X modifiers */
+#define XK_ANY_MOD    UINT_MAX
+#define XK_NO_MOD     0
+#define XK_SWITCH_MOD (1<<13)
+
+/* alpha */
+#define OPAQUE 0Xff
+#define USE_ARGB (alpha != OPAQUE && opt_embed == NULL)
+
+typedef XftGlyphFontSpec GlyphFontSpec;
+
+void draw(void);
+void drawregion(int, int, int, int);
+void run(void);
+
+void xbell(int);
+void xclipcopy(void);
+void xclippaste(void);
+void xhints(void);
+void xinit(void);
+void xloadcols(void);
+int xsetcolorname(int, const char *);
+void xloadfonts(char *, double);
+void xsetenv(void);
+void xsettitle(char *);
+void xsetpointermotion(int);
+void xseturgency(int);
+void xunloadfonts(void);
+void xresize(int, int);
+void xselpaste(void);
+unsigned long xwinid(void);
+void xsetsel(char *, Time);
