bdf3be62912ee99bc0381c4a82a756b1310a2c52
Mitch Weaver <mitch@wvr.sh>
2021-05-18 10:52:44 -0500

    ranger: clean up scope.sh

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -64,23 +64,17 @@ handle_extension() {
             7z l -p -- "${FILE_PATH}" && exit 5
             exit 1;;
 
-        # ## PDF
-        # pdf)
-        #     ## Preview as text conversion
-        #     pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - | \
-        #       fmt -w "${PV_WIDTH}" && exit 5
-        #     mutool draw -F txt -i -- "${FILE_PATH}" 1-10 | \
-        #       fmt -w "${PV_WIDTH}" && exit 5
-        #     exiftool "${FILE_PATH}" && exit 5
-        #     exit 1;;
+        pdf)
+            ## Preview as text conversion
+            mutool draw -F txt -i -- "${FILE_PATH}" 1-10 | \
+              fmt -w "${PV_WIDTH}" && exit 5
+            exit 1;;
 
-        ## OpenDocument
-        # odt|ods|odp|sxw)
+        odt|ods|odp|sxw)
         #     ## Preview as text conversion
         #     odt2txt "${FILE_PATH}" && exit 5
-        #     ## Preview as markdown conversion
-        #     pandoc -s -t markdown -- "${FILE_PATH}" && exit 5
-        #     exit 1;;
+            pandoc -s -t markdown -- "${FILE_PATH}" && exit 5
+            exit 1;;
 
         ## XLSX
         xlsx)
@@ -120,18 +114,6 @@ handle_image() {
 
     local mimetype="${1}"
     case "${mimetype}" in
-        ## SVG
-        # image/svg+xml|image/svg)
-        #     convert -- "${FILE_PATH}" "${IMAGE_CACHE_PATH}" && exit 6
-        #     exit 1;;
-
-        ## DjVu
-        # image/vnd.djvu)
-        #     ddjvu -format=tiff -quality=90 -page=1 -size="${DEFAULT_SIZE}" \
-        #           - "${IMAGE_CACHE_PATH}" < "${FILE_PATH}" \
-        #           && exit 6 || exit 1;;
-
-        ## Image
         image/*)
             local orientation
             orientation="$( identify -format '%[EXIF:Orientation]\n' -- "${FILE_PATH}" )"
@@ -141,16 +123,11 @@ handle_image() {
                 ## ...auto-rotate the image according to the EXIF data.
                 convert -- "${FILE_PATH}" -auto-orient "${IMAGE_CACHE_PATH}" && exit 6
             fi
-
-            ## `w3mimgdisplay` will be called for all images (unless overriden
-            ## as above), but might fail for unsupported types.
             exit 7;;
 
-        ## Video
-        # video/*)
-        #     # Thumbnail
-        #     ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
-        #     exit 1;;
+        video/*)
+            ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
+            exit 1;;
 
         ## PDF
         # application/pdf)
@@ -162,7 +139,6 @@ handle_image() {
         #              -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
         #         && exit 6 || exit 1;;
 
-
         ## ePub, MOBI, FB2 (using Calibre)
         # application/epub+zip|application/x-mobipocket-ebook|\
         # application/x-fictionbook+xml)
@@ -173,7 +149,7 @@ handle_image() {
         #         >/dev/null && exit 6
         #     exit 1;;
 
-        ## Font
+        # Font
         application/font*|application/*opentype)
             preview_png="/tmp/$(basename "${IMAGE_CACHE_PATH%.*}").png"
             if fontimage -o "${preview_png}" \
@@ -195,64 +171,42 @@ handle_image() {
             ;;
 
         ## Preview archives using the first image inside.
-        ## (Very useful for comic book collections for example.)
-        # application/zip|application/x-rar|application/x-7z-compressed|\
-        #     application/x-xz|application/x-bzip2|application/x-gzip|application/x-tar)
-        #     local fn=""; local fe=""
-        #     local zip=""; local rar=""; local tar=""; local bsd=""
-        #     case "${mimetype}" in
-        #         application/zip) zip=1 ;;
-        #         application/x-rar) rar=1 ;;
-        #         application/x-7z-compressed) ;;
-        #         *) tar=1 ;;
-        #     esac
-        #     { [ "$tar" ] && fn=$(tar --list --file "${FILE_PATH}"); } || \
-        #     { fn=$(bsdtar --list --file "${FILE_PATH}") && bsd=1 && tar=""; } || \
-        #     { [ "$rar" ] && fn=$(unrar lb -p- -- "${FILE_PATH}"); } || \
-        #     { [ "$zip" ] && fn=$(zipinfo -1 -- "${FILE_PATH}"); } || return
-        #
-        #     fn=$(echo "$fn" | python -c "import sys; import mimetypes as m; \
-        #             [ print(l, end='') for l in sys.stdin if \
-        #               (m.guess_type(l[:-1])[0] or '').startswith('image/') ]" |\
-        #         sort -V | head -n 1)
-        #     [ "$fn" = "" ] && return
-        #     [ "$bsd" ] && fn=$(printf '%b' "$fn")
-        #
-        #     [ "$tar" ] && tar --extract --to-stdout \
-        #         --file "${FILE_PATH}" -- "$fn" > "${IMAGE_CACHE_PATH}" && exit 6
-        #     fe=$(echo -n "$fn" | sed 's/[][*?\]/\\\0/g')
-        #     [ "$bsd" ] && bsdtar --extract --to-stdout \
-        #         --file "${FILE_PATH}" -- "$fe" > "${IMAGE_CACHE_PATH}" && exit 6
-        #     [ "$bsd" ] || [ "$tar" ] && rm -- "${IMAGE_CACHE_PATH}"
-        #     [ "$rar" ] && unrar p -p- -inul -- "${FILE_PATH}" "$fn" > \
-        #         "${IMAGE_CACHE_PATH}" && exit 6
-        #     [ "$zip" ] && unzip -pP "" -- "${FILE_PATH}" "$fe" > \
-        #         "${IMAGE_CACHE_PATH}" && exit 6
-        #     [ "$rar" ] || [ "$zip" ] && rm -- "${IMAGE_CACHE_PATH}"
-        #     ;;
+        # (Very useful for comic book collections for example.)
+         application/zip|application/x-rar|application/x-7z-compressed|\
+             application/x-xz|application/x-bzip2|application/x-gzip|application/x-tar)
+             local fn=""; local fe=""
+             local zip=""; local rar=""; local tar=""; local bsd=""
+             case "${mimetype}" in
+                 application/zip) zip=1 ;;
+                 application/x-rar) rar=1 ;;
+                 application/x-7z-compressed) ;;
+                 *) tar=1 ;;
+             esac
+             { [ "$tar" ] && fn=$(tar --list --file "${FILE_PATH}"); } || \
+             { fn=$(bsdtar --list --file "${FILE_PATH}") && bsd=1 && tar=""; } || \
+             { [ "$rar" ] && fn=$(unrar lb -p- -- "${FILE_PATH}"); } || \
+             { [ "$zip" ] && fn=$(zipinfo -1 -- "${FILE_PATH}"); } || return
+        
+             fn=$(echo "$fn" | python -c "import sys; import mimetypes as m; \
+                     [ print(l, end='') for l in sys.stdin if \
+                       (m.guess_type(l[:-1])[0] or '').startswith('image/') ]" |\
+                 sort -V | head -n 1)
+             [ "$fn" = "" ] && return
+             [ "$bsd" ] && fn=$(printf '%b' "$fn")
+        
+             [ "$tar" ] && tar --extract --to-stdout \
+                 --file "${FILE_PATH}" -- "$fn" > "${IMAGE_CACHE_PATH}" && exit 6
+             fe=$(echo -n "$fn" | sed 's/[][*?\]/\\\0/g')
+             [ "$bsd" ] && bsdtar --extract --to-stdout \
+                 --file "${FILE_PATH}" -- "$fe" > "${IMAGE_CACHE_PATH}" && exit 6
+             [ "$bsd" ] || [ "$tar" ] && rm -- "${IMAGE_CACHE_PATH}"
+             [ "$rar" ] && unrar p -p- -inul -- "${FILE_PATH}" "$fn" > \
+                 "${IMAGE_CACHE_PATH}" && exit 6
+             [ "$zip" ] && unzip -pP "" -- "${FILE_PATH}" "$fe" > \
+                 "${IMAGE_CACHE_PATH}" && exit 6
+             [ "$rar" ] || [ "$zip" ] && rm -- "${IMAGE_CACHE_PATH}"
+             ;;
     esac
-
-    # openscad_image() {
-    #     TMPPNG="$(mktemp -t XXXXXX.png)"
-    #     openscad --colorscheme="${OPENSCAD_COLORSCHEME}" \
-    #         --imgsize="${OPENSCAD_IMGSIZE/x/,}" \
-    #         -o "${TMPPNG}" "${1}"
-    #     mv "${TMPPNG}" "${IMAGE_CACHE_PATH}"
-    # }
-
-    # case "${FILE_EXTENSION_LOWER}" in
-    #     ## 3D models
-    #     ## OpenSCAD only supports png image output, and ${IMAGE_CACHE_PATH}
-    #     ## is hardcoded as jpeg. So we make a tempfile.png and just
-    #     ## move/rename it to jpg. This works because image libraries are
-    #     ## smart enough to handle it.
-    #     csg|scad)
-    #         openscad_image "${FILE_PATH}" && exit 6
-    #         ;;
-    #     3mf|amf|dxf|off|stl)
-    #         openscad_image <(echo "import(\"${FILE_PATH}\");") && exit 6
-    #         ;;
-    # esac
 }
 
 handle_mime() {
@@ -265,7 +219,7 @@ handle_mime() {
         ## Image
         image/*)
             ## Preview as text conversion
-            # img2txt --gamma=0.6 --width="${PV_WIDTH}" -- "${FILE_PATH}" && exit 4
+            img2txt --gamma=0.6 --width="${PV_WIDTH}" -- "${FILE_PATH}" && exit 4
             exiftool "${FILE_PATH}" && exit 5
             exit 1;;
         ## Video and audio
