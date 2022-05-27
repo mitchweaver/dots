1bc92d4948fbdc6fc116cf67ed7fe38e9e98d710
wvr <mitch@wvr.sh>
2022-05-27 07:22:07 -0500

    load fonts after

--- setup/fedora-workstation-setup.sh
+++ setup/fedora-workstation-setup.sh
@@ -102,6 +102,8 @@ dnf install -y \
 	dejavu-sans-fonts \
 	dejavu-sans-mono-fonts
 
+# load fonts after
+fc-cache -vf
 
 dnf install -y cabextract xorg-x11-font-utils fontconfig
 rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
