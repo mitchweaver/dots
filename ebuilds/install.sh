3b56933237d7ed5d49f3bd893790f3621a794181
wvr <mitch@wvr.sh>
2022-10-17 02:27:54 -0500

    ebuilds install.sh: fail safe if doesnt have eselect repository

--- ebuilds/install.sh
+++ ebuilds/install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 if ! grep '[mitchw]' /etc/portage/repos.conf/eselect-repo.conf >/dev/null ; then
-	eselect repository create mitchw
+	if grep 'app-eselect/eselect-repository' /var/lib/portage/world >/dev/null ; then
+		eselect repository create mitchw
+	else
+		>&2 echo 'You need to emerge eselect-repository.'
+		exit 1
+	fi
 fi
 
 rsync -avhu --progress --delete mitchw/ /var/db/repos/mitchw
