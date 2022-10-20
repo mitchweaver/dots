7f9ed9a71edbd366506889e6f75193f531a3fdd7
wvr <mitch@wvr.sh>
2022-10-20 04:24:28 -0500

    script updates

--- scripts/install-vim-plug.sh
+++ scripts/install-vim-plug.sh
@@ -6,3 +6,12 @@ if [ ! -e ~/.vim/autoload/plug.vim ] ; then
     curl -#fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
+if [ ! -f ~/.vimrc ] ; then
+    if [ -f ../.vimrc ]; then
+        cp -v ../.vimrc ~/.vimrc
+        if [ -f ~/.bashrc ] ; then
+            echo 'alias v=vim' >> ~/.bashrc
+        fi
+    fi
+fi
