b24d22e138187ca9eb38ac7beca033dfcdeeedf7
wvr <mitch@wvr.sh>
2022-10-20 04:56:31 -0500

    fix

--- scripts/grab-vim.sh
+++ scripts/grab-vim.sh
@@ -7,11 +7,14 @@ if [ ! -e ~/.vim/autoload/plug.vim ] ; then
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+if [ ! -e ~/.config/nvim ] ; then
+    mkdir -p ~/.config
+    cp -r ../.config/nvim ~/.config/
+fi
+
 if [ ! -f ~/.vimrc ] ; then
-    if [ -f ../.vimrc ]; then
-        cp -v ../.vimrc ~/.vimrc
-        if [ -f ~/.bashrc ] ; then
-            echo 'alias v=vim' >> ~/.bashrc
-        fi
+    cp -v ../.vimrc ~/.vimrc
+    if [ -f ~/.bashrc ] ; then
+        echo 'alias v=vim' >> ~/.bashrc
     fi
 fi
