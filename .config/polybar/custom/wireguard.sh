998d3cb5a9b46ad71dba0e452a2ecd1c0d110ad7
wvr <mitch@wvr.sh>
2022-10-21 10:49:46 -0500

    polybar: add wg module

---
+++ .config/polybar/custom/wireguard.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+ints='wg0 wg1 wg2'
+ifconfig=$(ifconfig)
+
+for int in $ints ; do
+    if printf '%s\n' "$ifconfig" | grep "$int" >/dev/null ; then
+        wg="$int"
+        break
+    fi
+done
+
+color_fmt=%{F#F0C674}
+clear_fmt=%{F-}
+
+if [ "$wg" ] ; then
+    printf '%s%s %s' "${color_fmt}" "${clear_fmt}" "$wg"
+else
+    printf '%s%s None' "${color_fmt}" "${clear_fmt}"
+# 
+# 
+fi
