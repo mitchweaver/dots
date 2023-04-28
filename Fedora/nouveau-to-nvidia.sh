0a86e39b30236b9cfe0a1d18367cd44c0478669c
wvr <mitch@wvr.sh>
2023-04-27 20:19:57 -0500

    fedora: nouveau-to-nvidia.sh

---
+++ Fedora/nouveau-to-nvidia.sh
@@ -0,0 +1,5 @@
+sudo dnf remove -y xorg-x11-drv-nouveau
+
+sudo dnf update -y # and reboot if you are not on the latest kernel
+sudo dnf install akmod-nvidia # rhel/centos users can use kmod-nvidia instead
+sudo dnf install xorg-x11-drv-nvidia-cuda #optional for cuda/nvdec/nvenc support
